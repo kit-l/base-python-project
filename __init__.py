44b43f066d524ba128b6074aa69502e55afce865
kit <noreply@example.com>
2022-09-04 09:25:10 -0600

    initial commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "0.0.1"
+__description__ = """
+A long description of the project's purpose.
+"""
---
+++ src/example_module/__init__.py
@@ -0,0 +1,5 @@
+"""
+A long description of the project's purpose.
+"""
+
+__version__ = "0.0.1"
